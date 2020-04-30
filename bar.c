abffd293e74fd0283753dc1ff5c4b5876f390355
Rodolfo Adamshuk <rodolfoa@utfpr.edu.br>
2020-04-30 18:38:36 -0300

    Função soma adicionada

--- bar.c
+++ bar.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+void soma(int a, int b){
+	int soma;
+	soma=a+b;
+	printf("%d\n", soma);
+}
+
 int main(){
 	int a, b;
 	scanf("%d %d", &a, &b);
-	printf("%d %d", a, b);
+	printf("%d %d\n", a, b);
+	soma(a, b);
 }
