4793e0e301099d147f1db1beb92b7c376cfadb00
Rodolfo Adamshuk <rodolfoa@utfpr.edu.br>
2020-05-03 17:16:28 -0300

    Função multiplicação adicionada

--- bar.c
+++ bar.c
@@ -6,9 +6,16 @@ void soma(int a, int b){
 	printf("%d\n", soma);
 }
 
+void multiplicacao(int a, int b){
+	int mult;
+	mult=a*b;
+	printf("%d\n", mult);
+}
+
 int main(){
 	int a, b;
 	scanf("%d %d", &a, &b);
 	printf("%d %d\n", a, b);
 	soma(a, b);
+	multiplicacao(a, b);
 }
