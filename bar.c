8cc0e6ee8afa76aa70132dea6d404224d91d09fa
Rodolfo Adamshuk <rodolfoa@utfpr.edu.br>
2020-05-03 16:04:01 -0300

    Função subtração adicionada

--- bar.c
+++ bar.c
@@ -6,9 +6,16 @@ void soma(int a, int b){
 	printf("%d\n", soma);
 }
 
+void subtracao(int a, int b){
+	int sub;
+	sub=a-b;
+	printf("%d\n", sub);
+}
+
 int main(){
 	int a, b;
 	scanf("%d %d", &a, &b);
 	printf("%d %d\n", a, b);
 	soma(a, b);
+	subtracao(a, b);
 }
